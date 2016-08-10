833b68cd1559bcf35afc1958559729ea46753c3d
longfe1yang <yanglongfei71937315@gmail.com>
2016-08-10 10:38:16 +0800

    增加了一级目录和二级目录的断点续传功能
    改进

--- main.py
+++ main.py
@@ -10,27 +10,36 @@ def get_all_links_from(channel):
         get_links_from(channel, i)
 
 
-# 这个get_index_url有去重功能
+# 这个get_index_url有去重功能，也就是说
+# 这个get_start()运行无论多少次，都不会加入重复信息
 def get_start():
     start_url = 'http://bj.ganji.com/wu/'
     host = 'http://bj.ganji.com'
     get_index_url(start_url, host)
 
 
-all_url = [i['url'] for i in models.second_grade.find()]
-exist_url = [i['url'] for i in models.third_grade.find()]
-rst_urls = set(all_url) - set(exist_url)
+# 从二级拿出数据和三级（详细页面）中所有url对比，给出来剩下没有爬取的
+# 详细页面url
+def info_rst():
+    all_url = [i['url'] for i in models.second_grade.find()]
+    exist_url = [i['url'] for i in models.third_grade.find()]
+    return set(all_url) - set(exist_url)
+
+
+# 从一级索引拿出page_url和二级索引做对比，给出来一级下面没有爬取的
+def url_rst():
+    page_url = [i['page_url'] for i in models.first_grade.find()]
+    exist_page_url = [i['channel'] for i in models.second_grade.find()]
+    return set(page_url) - set(exist_page_url)
 
 
 def main():
     # get_start()
     pool = Pool()
-    all_channel = [i['page_url'] for i in models.first_grade.find()]
-    # pool.map(get_all_links_from, all_channel)
-    pool.map(get_item_info, rst_urls)
+    pool.map(get_all_links_from, url_rst())
+    pool.map(get_item_info, info_rst())
     pool.close()
     pool.join()
-    # print(all_channel)
 
 if __name__ == '__main__':
     main()
