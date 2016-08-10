5caaeb5bc9d85ec9eacd5f66ad5004683a08d6e2
longfe1yang <yanglongfei71937315@gmail.com>
2016-08-10 15:31:03 +0800

    测试完成
    有去重功能和断点续传功能

--- channels.py
+++ channels.py
@@ -4,7 +4,10 @@ import models
 import time
 
 
-# 最后权衡了一下还是把验重合成进了函数里面，这样import一个函数就可以了，
+def page_url_exist(page_url):
+    return page_url in [i['page_url'] for i in models.first_grade.find()]
+
+
 # 最大化利用一致性
 def get_index_url(url, host):
     res = requests.get(url)
@@ -12,13 +15,11 @@ def get_index_url(url, host):
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'lxml')
     titles = soup.select('#wrapper > div.content > div > div > dl.fenlei > dt > a')
-    totals = [i['page_url'] for i in models.first_grade.find()]
-    print(totals)
     for i in titles:
         title = i.get_text()
         url = i.get('href')
         page_url = host + url
-        if page_url not in totals:
+        if not page_url_exist(page_url):
             data = {
                 'title': title,
                 'page_url': page_url
--- models.py
+++ models.py
@@ -5,3 +5,7 @@ collection = client['ganji']
 first_grade = collection['page_url']
 second_grade = collection['preview_url']
 third_grade = collection['info_details']
+
+# b = len(set([i['url'] for i in second_grade.find()]))
+# a = second_grade.find().count()
+# print(a, b)
