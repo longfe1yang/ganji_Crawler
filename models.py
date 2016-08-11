2078555bd571d0827794bb3d5abd00a37b4d6595
longfe1yang <yanglongfei71937315@gmail.com>
2016-08-11 20:49:37 +0800

    一点小修改

--- models.py
+++ models.py
@@ -7,6 +7,10 @@ second_grade = collection['preview_url']
 third_grade = collection['info_details']
 
 if __name__ == '__main__':
-    b = len(set([i['page_url'] for i in first_grade.find()]))
-    a = first_grade.find().count()
+    b = len(set([i['url'] for i in third_grade.find()]))
+    a = second_grade.find().count()
     print(a, b)
+    # a = [i['channel'] for i in second_grade.find()]
+    # b = 'http://bj.ganji.com/xianzhilipin/'
+    # if b in a:
+    #     print(True)
