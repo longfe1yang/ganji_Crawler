b8632040949ac6e20682dd7f6ff16273bc4dec50
longfe1yang <yanglongfei71937315@gmail.com>
2016-08-11 22:00:35 +0800

    排版优化了一些

--- pages_parsing.py
+++ pages_parsing.py
@@ -8,63 +8,68 @@ def formatted(string):
     return string.replace("\n", "").replace("\t", "").replace(" ", "")
 
 
-def url_exist(url):
-    return url in [i['url'] for i in models.second_grade.find()]
+def url_save(url, channel):
+    urls = [i['url'] for i in models.second_grade.find()]
+    if url not in urls:
+        data = {
+            'url': url,
+            'channel': channel
+        }
+        print(data)
+        models.second_grade.save(data)
+    else:
+        return None
 
 
 def get_links_from(channel, pages):
     list_view = '{}o{}/'.format(channel, pages)
     wb_data = requests.get(list_view)
     time.sleep(2)
-    soup = BeautifulSoup(wb_data.text, 'lxml')
-    # 判断页面是否存在，找的是<div class="pageBox">
-    # 有，页面就存在，没有，页面就不存在
-    if soup.find('div', 'pageBox'):
-        urls = soup.select('[class=ft-tit]')
-        for u in urls:
-            url = u.get('href')
-            # 去重判断
-            if not url_exist(url):
-                data = {
-                    'url': url,
-                    'channel': channel
-                }
-                print(data)
-                models.second_grade.save(data)
-            else:
-                continue
+    if wb_data.status_code == 200:
+        soup = BeautifulSoup(wb_data.text, 'lxml')
+        # 判断页面是否存在，找的是<div class="pageBox">
+        # 有，页面就存在，没有，页面就不存在
+        if soup.find('div', 'pageBox'):
+            urls = soup.select('[class=ft-tit]')
+            for u in urls:
+                url = u.get('href')
+                url_save(url, channel)
+        else:
+            return
 
 
 def item_info_exist(url):
     return url in [i['url'] for i in models.third_grade.find()]
 
 
+def info_page_exist(soup):
+    page_404_1 = soup.select('div.leftBox > div:nth-of-type(4) > div > ul > li:nth-of-type(1) > span:nth-of-type(1)')
+    return len(page_404_1) == 0 and soup.find('p', 'error-tips1') is None
+
+
 def get_item_info(url):
     if not item_info_exist(url):
         wb_data = requests.get(url)
+        if wb_data.status_code != 200:
+            return None
         time.sleep(2)
         soup = BeautifulSoup(wb_data.text, 'lxml')
-        page_404_1 = soup.select('div.leftBox > div:nth-of-type(4) > div > ul > li:nth-of-type(1) > span:nth-of-type(1)')
-        if len(page_404_1) == 0 and soup.find('p', 'error-tips1') is None:
-            title = soup.find('h1', 'title-name').text
-            tm = soup.find('i', 'pr-5').text.strip()
-            typ = soup.select('div.leftBox > div:nth-of-type(3) > div > ul > li:nth-of-type(1) > span > a')[0].text
-            price = soup.find('i', 'f22 fc-orange f-type').text
-            p = soup.select('div.leftBox > div:nth-of-type(3) > div > ul > li:nth-of-type(3) > a')
-            place = ''
-            for i in range(len(p)):
-                place += p[i].get_text()
+        if info_page_exist(soup):
+            tm = soup.select('.pr-5')
+            typ = soup.select('div.leftBox > div:nth-of-type(3) > div > ul > li:nth-of-type(1) > span > a')
+            price = soup.select('.f22.fc-orange.f-type')
+            place = soup.select('ul.det-infor > li:nth-of-type(3) > a')
             state = soup.select('div.second-dt-bewrite > ul > li')
             data = {
                 'url': url,
-                'title': title,
-                'time': tm,
-                'type': typ,
-                'price': price,
-                'place': place,
+                'title': soup.title.text.strip(),
+                'time': tm[0].text.strip().split(' ')[0] if len(tm) > 0 else "",
+                'type': typ[0].text if len(typ) > 0 else '',
+                'price': price[0].text.strip() if len(price) > 0 else 0,
+                'place': [area.text.strip() for area in place if area.text.strip() != "-"],
                 'state': formatted(state[0].get_text() if len(state) != 0 else "")
             }
-            models.third_grade.save(data)
+            # models.third_grade.save(data)
             print(data)
         else:
             pass
@@ -73,4 +78,4 @@ def get_item_info(url):
 
 
 if __name__ == '__main__':
-    get_item_info('http://bj.ganji.com/jiaju/2215310340x.htm')
+    get_item_info('http://bj.ganji.com/shuma/1868232000x.htm')
